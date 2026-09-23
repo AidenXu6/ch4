723a8e43eaf1e0dd4c0d6e3d1cb05955f46514b0
AidenXu6 <1548293@learn.vsb.bc.ca>
2026-09-23 14:26:27 -0700

    Add files via upload

---
+++ Difftime.java
@@ -0,0 +1,35 @@
+public class Difftime{
+	public static void main(String[]args){
+		int day=23;
+		int year=2026;
+		String weekday="Wednesday";
+		String month="September";
+		printeurtime(weekday,day,month,year);
+		printusatime(weekday,month,day,year);
+		
+		
+		
+		
+}
+	public static void printeurtime(String weekday,int day,String month,int year){
+		System.out.println(weekday+" "+day+" "+month+" "+year);
+		
+		
+		
+}
+
+	public static void printusatime(String weekday,String month,int day,int year){
+	    System.out.println(weekday+" "+month+" "+day+" "+year);
+	
+	
+	}
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	

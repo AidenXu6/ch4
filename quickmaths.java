6cef23e4484db6d6b7695aa52e22ef9e040ab962
AidenXu6 <1548293@learn.vsb.bc.ca>
2026-09-24 14:55:24 -0700

    Add files via upload

---
+++ quickmaths.java
@@ -0,0 +1,21 @@
+public class quickmaths{
+	public static double multadd (double a,double b,double c){
+		return a*b+c;
+		
+	}
+	
+	
+	public static void main (String [] args){
+		System.out.print(multadd(Math.cos(Math.PI/4),0.5,Math.sin(Math.PI/4)));
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
